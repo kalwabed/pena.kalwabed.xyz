a9e5705fdfca546ee280114b3f615e756d01523e
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-05-11 19:49:42 +0700

    chore: update article, update deps

--- src/lib/components/blog/image.svelte
+++ src/lib/components/blog/image.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <figure>
-  <img {src} alt="Omnahe uwong" class="rounded-lg" decoding="async" loading="lazy" />
+  <img {src} alt="Ini gambar" class="rounded-lg w-full" height="100" width="100" decoding="async" loading="lazy" />
   <caption class="text-xs inline-flex space-x-1">
     {@html caption}
   </caption>
