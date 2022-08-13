0748da8d4d14792a8e4ae28c41328a16c4a544aa
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-08-13 07:27:00 +0700

    centering image caption

--- src/lib/components/blog/image.svelte
+++ src/lib/components/blog/image.svelte
@@ -3,7 +3,7 @@
   export let caption;
 </script>
 
-<figure>
+<figure class="text-center">
   <img {src} alt="Ini gambar" class="rounded-lg w-full" height="100" width="100" decoding="async" loading="lazy" />
   <caption class="text-xs inline-flex space-x-1">
     {@html caption}
