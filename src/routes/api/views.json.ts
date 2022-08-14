8dfdc353e14f4797aa27f09a62087f7ad81c19df
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-08-14 14:13:14 +0700

    separating method GET and POST

--- src/routes/api/views.json.ts
+++ src/routes/api/views.json.ts
@@ -1,5 +1,3 @@
-import type { RequestEvent } from '@sveltejs/kit';
-
 import { prisma } from '$lib/providers/prisma';
 
 export type Views = {
@@ -14,54 +12,6 @@ export async function GET() {
     return {
       status: 200,
       body: { stats: postViewsCount }
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      status: 500,
-      body: {
-        error: {
-          message: error.message,
-          code: error.errorCode,
-          clientVersion: error.clientVersion
-        }
-      }
-    };
-  }
-}
-
-export async function POST({ request }: RequestEvent) {
-  const req = await request.json();
-  const slug = req.slug as string;
-
-  if (!slug) {
-    return {
-      status: 400,
-      body: {
-        message: 'slug is required'
-      }
-    };
-  }
-
-  try {
-    const postViewsCount = await prisma.views.upsert({
-      where: {
-        slug
-      },
-      create: {
-        slug,
-        count: 1
-      },
-      update: {
-        count: {
-          increment: 1
-        }
-      }
-    });
-
-    return {
-      status: 201,
-      body: postViewsCount
     };
   } catch (error) {
     console.error(error);
