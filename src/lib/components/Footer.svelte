447cd2a155b65435f8c4c2b5e7c5e1737c7a0636
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-10-19 06:12:32 +0700

    feat: adding prefetch

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="px-4 py-8 md:p-8 w-full mt-auto">
   <div class="flex justify-center w-full space-x-8 text-gray-600 text-lg">
-    <a class="hover:text-blue-500" href="/">/home</a>
-    <a class="hover:text-blue-500" href="/guestbook">/guestbook</a>
+    <a class="hover:text-blue-500" href="/" data-sveltekit-prefetch>/home</a>
+    <a class="hover:text-blue-500" href="/guestbook" data-sveltekit-prefetch>/guestbook</a>
     <a href="https://www.kalwabed.xyz" class="hover:text-blue-500" rel="noopener noreferrer" target="_blank"
       >kalwabed.xyz</a
     >
