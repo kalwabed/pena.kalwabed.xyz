bcce7b86a9040ad05a3900446a1dcb9cb8ca1c31
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-10-19 20:39:19 +0700

    feat: dynamic og image

--- src/lib/configs/app.ts
+++ src/lib/configs/app.ts
@@ -1,7 +1,8 @@
 export default {
   siteName: 'Pena',
   siteUrl: 'https://pena.kalwabed.xyz',
-  siteDescription: 'Koordinat tulisan-tulisan abstrak Kalwabed',
+  siteDescription:
+    'Tempat Kalwabed Rizki menuangkan tulisan-tulisan yang berisi tentang pengalaman, ilmu, dan inspirasi.',
   contact: 'https://twitter.com/kalwabedrzk',
   email: 'kalwabed@gmail.com',
   keywords: ['kisah', 'inspirasi', 'artikel']
