1fc436df7e85325be967d1097774f489a69445af
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-06-24 13:25:08 +0700

    fix: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,19 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint'],
+  plugins: ['svelte4', '@typescript-eslint'],
   ignorePatterns: ['*.cjs'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte4/svelte4' }],
   settings: {
-    'svelte3/typescript': () => require('typescript')
+    'svelte4/typescript': () => require('typescript'),
   },
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   env: {
     browser: true,
     es2017: true,
-    node: true
-  }
+    node: true,
+  },
 };
