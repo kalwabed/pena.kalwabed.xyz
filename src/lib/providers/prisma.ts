ebf6c05450dcd398aa9e92de3ea96accb0723a48
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-12-24 19:57:52 +0700

    ref: get rid global

--- src/lib/providers/prisma.ts
+++ src/lib/providers/prisma.ts
@@ -1,15 +1,15 @@
 import Prisma, * as PrismaScope from '@prisma/client';
 import type { PrismaClient } from '@prisma/client';
 
-export let prisma: PrismaClient;
+// export let prisma: PrismaClient;
 
 const prismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
 
-if (import.meta.env.PROD) {
-  prisma = new prismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new prismaClient();
-  }
-  prisma = global.prisma;
-}
+// if (import.meta.env.PROD) {
+export default new prismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new prismaClient();
+//   }
+//   prisma = global.prisma;
+// }
