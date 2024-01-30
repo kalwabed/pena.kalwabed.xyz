33094787921698485b97fb3808fcac88755a7a01
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-01-30 19:07:44 +0700

    fix: tags overlap

--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -13,7 +13,7 @@
 <h1 class="text-4xl font-bold leading-relaxed">🪢 Tags</h1>
 <p class="text-gray-500 dark:text-mauveEleven">Temukan tulisan-tulisan berdasarkan Tag di bawah.</p>
 
-<ul class="flex gap-4 mt-6">
+<ul class="flex flex-wrap gap-4 mt-6">
   {#each tags as tag}
     <li>
       <Tag {tag} />
