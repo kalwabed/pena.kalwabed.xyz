77655fb7f4bec10fe4645b149d0f60a653961a6f
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-11-11 21:08:30 +0700

    fix: prisma undefined

--- src/lib/providers/prisma.ts
+++ src/lib/providers/prisma.ts
@@ -2,14 +2,14 @@ import Prisma, * as PrismaScope from '@prisma/client';
 import type { PrismaClient } from '@prisma/client';
 
 export let prisma: PrismaClient;
-let global: { prisma: PrismaClient };
+let global: { prisma: PrismaClient } = { prisma: null };
 
 const prismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
 
 if (import.meta.env.PROD) {
   prisma = new prismaClient();
 } else {
-  if (!global.prisma) {
+  if (!global?.prisma) {
     global.prisma = new prismaClient();
   }
   prisma = global.prisma;
